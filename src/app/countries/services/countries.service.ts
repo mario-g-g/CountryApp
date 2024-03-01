fd5ba03454da6e833a0ec060f9efc8bb46a4654e
mario.gil <mario.gil@civica-soft.com>
2024-03-01 09:20:42 +0100

    Siguiendo seccion 9

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -12,8 +12,33 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   searchCapital( term: string ): Observable<Country[]>{
+
     const url = `${ this.apiUrl }/capital/${ term }`
+
     return this.http.get<Country[]>(url)
+    .pipe(
+      catchError( error => of([]) )
+    );
   }
+
+  searchCountry( term: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/name/${ term }`
+
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError( error => of([]) )
+    );
+  }
+
+  searchRegion( region: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/region/${ region }`
+
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError( error => of([]) )
+    );
+  }
+
+
 
 }
